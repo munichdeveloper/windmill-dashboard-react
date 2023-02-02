ad861a9886728904a95a223bea02838500c7a55d
munich developer <johannes.hinkov@gmail.com>
2023-02-02 01:07:16 +0100

    local dev

--- src/api/ApiClient.js
+++ src/api/ApiClient.js
@@ -2,29 +2,37 @@ import axios from "axios";
 
 export async function getHostConfig() {
   return await axios
-    .get(`/scpconfig/host`)
+    .get(`/api/scpconfig/host`)
 }
 
 export async function getModuleStatus(name) {
-  return await axios.get(`/status/${name}`)
+  return await axios.get(`/api/status/${name}`)
 }
 
 export async function getWalletBalances() {
-  return await axios.get(`/wallet`)
+  return await axios.get(`/api/wallet`)
 }
 
 export async function getLogEntriesByKey(key) {
-    return await axios.get(`/log/${key}`)
+    return await axios.get(`/api/log/${key}`)
 }
 
 export async function getConfigValue(key) {
-    return await axios.get(`/setting/${key}`)
+    return await axios.get(`/api/setting/${key}`)
 }
 
 export async function getConfigValues() {
-    return await axios.get('setting');
+    return await axios.get('/api/setting');
 }
 
 export async function setConfigValue(key,value) {
-    return await axios.put(`/setting/${key}`, value)
+    const config = {
+        headers: {
+            'Content-Type': "plain/text"
+        }
+    }
+
+    console.log('value.length', value.length)
+
+    return await axios.put(`/api/setting/${key}`, value, config)
 }
